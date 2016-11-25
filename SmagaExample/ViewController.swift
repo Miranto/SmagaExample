c351c3726aa396d87bf419d9c6f7e1b8f23b5a29
Mirczenzo <mateusz.mirkowski@visentcoders.com>
2016-11-25 15:35:29 +0100

    first commit

---
+++ SmagaExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmagaExample
+//
+//  Created by grzesiek on 25/11/2016.
+//  Copyright © 2016 Mateusz Mirkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
